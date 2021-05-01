41a69736445404ad2e40c2737261a013caa52812
daniel-ablin <78722263+daniel-ablin@users.noreply.github.com>
2021-05-01 10:14:22 +0300

    Update and rename HW1_ID1_ID2.py to KNN_model.py

--- KNN_model.py
+++ KNN_model.py
@@ -23,20 +23,11 @@ class KnnClassifier:
         self.k = k
         self.p = p
 
-        # TODO - Place your student IDs here. Single submitters please use a tuple like so: self.ids = (123456789,)
-        self.ids = (203967443, 987654321)
-
     def fit(self, X: np.ndarray, y: np.ndarray) -> None:
         """
         This method trains a k-NN classifier on a given training set X with label set y.
 
-        :param X: A 2-dimensional numpy array of m rows and d columns. It is guaranteed that m >= 1 and d >= 1.
-            Array datatype is guaranteed to be np.float32.
-        :param y: A 1-dimensional numpy array of m rows. it is guaranteed to match X's rows in length (|m_x| == |m_y|).
-            Array datatype is guaranteed to be np.uint8.
         """
-
-        # TODO - your code here
         if X.ndim == 1: X = np.reshape(X, (1,X.shape[0]))
         self.X_train = X
         self.Y_train = y
@@ -45,15 +36,12 @@ class KnnClassifier:
         """
         This method predicts the y labels of a given dataset X, based on a previous training of the model.
         It is mandatory to call KnnClassifier.fit before calling this method.
-
-        :param X: A 2-dimensional numpy array of m rows and d columns. It is guaranteed that m >= 1 and d >= 1.
-            Array datatype is guaranteed to be np.float32.
-        :return: A 1-dimensional numpy array of m rows. Should be of datatype np.uint8.
+        
         """
 
-        # TODO - your code here
         # reshape dim to 2 if np shape is 1
         if X.ndim == 1: X = np.reshape(X, (1,X.shape[0]))
+        
         #Minkowski distance calculation - temp[i,j] is the distance between i and j
         temp = np.power(np.sum(np.power(np.abs(X[:,np.newaxis]-self.X_train), self.p), axis = 2), 1/self.p)
         
@@ -69,15 +57,11 @@ class KnnClassifier:
         # sort made the most_common function encounter neighbors based on the tie breaking rules
         prediction = np.apply_along_axis(get_class, 1, self.Y_train[idx[:,:self.k]])
         return prediction[:,0,0]
-
-        ### Example code - don't use this:
-        # return np.random.randint(low=0, high=2, size=len(X), dtype=np.uint8)
-
+    
 
 def main():
 
     print("*" * 20)
-    print("Started HW1_ID1_ID2.py")
     # Parsing script arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('csv', type=str, help='Input csv file path')
@@ -90,7 +74,6 @@ def main():
 
     print("Initiating KnnClassifier")
     model = KnnClassifier(k=args.k, p=args.p)
-    print(f"Student IDs: {model.ids}")
     print(f"Loading data from {args.csv}...")
     data = pd.read_csv(args.csv, header=None)
     print(f"Loaded {data.shape[0]} rows and {data.shape[1]} columns")
